193e9e05c5da45ca740c6a1280bfdaaca62ae508
gioparra91 <gioparra91@hotmail.it>
2022-03-07 15:18:39 +0000

    main instruction in new repository

---
+++ mainInstructions.js
@@ -0,0 +1,17 @@
+//// SETTING STEP: node, truffle, ganache, git correct version
+// type below in console:
+// node -v
+// git --version
+// npm install -g truffle@5.0.2
+// truffle version
+// truffle init --> notice the folders and config files got created
+// create new file called package.json
+
+//// CREATE SC STEP
+// create the sc by typing in console "truffle compile": notice in the "build folder"
+// notice the two "sc json definition" instead of .sol
+// the json file is the ABI file = abstract binary interface and it contains the bytecode version of sc
+
+//// TRUFFLE CONFIG
+// copy paste config in truffle-config.js to connect to ganache (blockchain id step)
+
