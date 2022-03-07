de4cf1e5d490b075ab5d947db0f74528ec599e69
gioparra91 <gioparra91@hotmail.it>
2022-03-07 15:56:48 +0000

    first tutorial done

---
+++ main.js
@@ -0,0 +1,38 @@
+//// SETTING STEP: node, truffle, ganache, git correct version
+// create repository in git, clone in vscode with ctrl+shift+p git:clone and choose foolder in local pc
+// stage, commit and push changes at the end of the day
+// type below in console:
+// node -v
+// git --version
+// npm install -g truffle@5.0.2
+// truffle version
+// truffle init --> notice the folders and config files got created
+// create new file called package.json with dependencies and copy paste from his github
+// npm install --> this will isntall all dependencies (make sure all versions aligned to his)
+
+//// CREATE SC STEP
+// create the sc you want under "contracts"
+// create the sc by typing in console "truffle compile": notice in the "build folder"
+// notice the two "sc json definition" instead of .sol
+// the json file is the ABI file = abstract binary interface and it contains the bytecode version of sc
+
+//// TRUFFLE CONFIG
+// copy paste config in truffle-config.js to connect to ganache (blockchain id step)
+
+//// CREATE MIGRATION FILE TO GET SC ONTO BC
+// migrating a sc is like migrating a db.
+// migrating a db means chg the state of the db with new tables etc
+// migrating a sc means chg the state of the bc itself, with a new sc
+// create a 2nd migration file.js with the new state to be deployed
+// make sure ganache is open; in console "truffle migrate"
+// deploying sc costs gas!
+
+//// GET INFO ABOUT SMARTCONTRACT
+// "truffle console" in console
+// "todoList = await TodoList.deployed()" Todolist is name var created in migration
+// "todoList.address" and "takeCount = await app.taskCount()"
+
+//// GIT: use have vscode and you previously created the repo, dont follow below
+// git init
+// git add .
+// git commit -am "project set up"
